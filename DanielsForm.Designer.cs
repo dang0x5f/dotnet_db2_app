8800e9540207fc03ad7c50ca75ab4ea494afa573
dang0x5f <dannypgehrz@gmail.com>
2024-09-06 21:22:59 -0500

    Adding form design and form class files + images of running application.

---
+++ DanielsForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace PixisAir_Team3_dg
+{
+    partial class DanielsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DB2DataListBox = new System.Windows.Forms.ListBox();
+            this.ResHistButton = new System.Windows.Forms.Button();
+            this.ResHistGroupBox = new System.Windows.Forms.GroupBox();
+            this.CarTableGroupBox = new System.Windows.Forms.GroupBox();
+            this.ModelTextBox = new System.Windows.Forms.TextBox();
+            this.CarsButton = new System.Windows.Forms.Button();
+            this.MakeTextBox = new System.Windows.Forms.TextBox();
+            this.IDTextBox = new System.Windows.Forms.TextBox();
+            this.ModelLabel = new System.Windows.Forms.Label();
+            this.MakeLabel = new System.Windows.Forms.Label();
+            this.IDLabel = new System.Windows.Forms.Label();
+            this.AddCarButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ResHistGroupBox.SuspendLayout();
+            this.CarTableGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DB2DataListBox
+            // 
+            this.DB2DataListBox.FormattingEnabled = true;
+            this.DB2DataListBox.ItemHeight = 14;
+            this.DB2DataListBox.Location = new System.Drawing.Point(13, 12);
+            this.DB2DataListBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.DB2DataListBox.Name = "DB2DataListBox";
+            this.DB2DataListBox.Size = new System.Drawing.Size(588, 242);
+            this.DB2DataListBox.TabIndex = 0;
+            // 
+            // ResHistButton
+            // 
+            this.ResHistButton.Location = new System.Drawing.Point(15, 21);
+            this.ResHistButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ResHistButton.Name = "ResHistButton";
+            this.ResHistButton.Size = new System.Drawing.Size(175, 25);
+            this.ResHistButton.TabIndex = 1;
+            this.ResHistButton.Text = "List All Reservations";
+            this.ResHistButton.UseVisualStyleBackColor = true;
+            this.ResHistButton.Click += new System.EventHandler(this.RESHISTButton_Click);
+            // 
+            // ResHistGroupBox
+            // 
+            this.ResHistGroupBox.Controls.Add(this.ResHistButton);
+            this.ResHistGroupBox.Location = new System.Drawing.Point(400, 260);
+            this.ResHistGroupBox.Name = "ResHistGroupBox";
+            this.ResHistGroupBox.Size = new System.Drawing.Size(202, 62);
+            this.ResHistGroupBox.TabIndex = 2;
+            this.ResHistGroupBox.TabStop = false;
+            this.ResHistGroupBox.Text = "Reservation History";
+            // 
+            // CarTableGroupBox
+            // 
+            this.CarTableGroupBox.Controls.Add(this.ModelTextBox);
+            this.CarTableGroupBox.Controls.Add(this.CarsButton);
+            this.CarTableGroupBox.Controls.Add(this.MakeTextBox);
+            this.CarTableGroupBox.Controls.Add(this.IDTextBox);
+            this.CarTableGroupBox.Controls.Add(this.ModelLabel);
+            this.CarTableGroupBox.Controls.Add(this.MakeLabel);
+            this.CarTableGroupBox.Controls.Add(this.IDLabel);
+            this.CarTableGroupBox.Controls.Add(this.AddCarButton);
+            this.CarTableGroupBox.Location = new System.Drawing.Point(12, 260);
+            this.CarTableGroupBox.Name = "CarTableGroupBox";
+            this.CarTableGroupBox.Size = new System.Drawing.Size(381, 95);
+            this.CarTableGroupBox.TabIndex = 3;
+            this.CarTableGroupBox.TabStop = false;
+            this.CarTableGroupBox.Text = "Cars Table";
+            // 
+            // ModelTextBox
+            // 
+            this.ModelTextBox.Location = new System.Drawing.Point(192, 26);
+            this.ModelTextBox.Name = "ModelTextBox";
+            this.ModelTextBox.Size = new System.Drawing.Size(84, 22);
+            this.ModelTextBox.TabIndex = 10;
+            // 
+            // CarsButton
+            // 
+            this.CarsButton.Location = new System.Drawing.Point(130, 60);
+            this.CarsButton.Name = "CarsButton";
+            this.CarsButton.Size = new System.Drawing.Size(120, 23);
+            this.CarsButton.TabIndex = 0;
+            this.CarsButton.Text = "List All Cars";
+            this.CarsButton.UseVisualStyleBackColor = true;
+            this.CarsButton.Click += new System.EventHandler(this.CarsButton_Click);
+            // 
+            // MakeTextBox
+            // 
+            this.MakeTextBox.Location = new System.Drawing.Point(102, 26);
+            this.MakeTextBox.Name = "MakeTextBox";
+            this.MakeTextBox.Size = new System.Drawing.Size(84, 22);
+            this.MakeTextBox.TabIndex = 9;
+            // 
+            // IDTextBox
+            // 
+            this.IDTextBox.Location = new System.Drawing.Point(12, 26);
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(84, 22);
+            this.IDTextBox.TabIndex = 8;
+            // 
+            // ModelLabel
+            // 
+            this.ModelLabel.AutoSize = true;
+            this.ModelLabel.Location = new System.Drawing.Point(208, 12);
+            this.ModelLabel.Name = "ModelLabel";
+            this.ModelLabel.Size = new System.Drawing.Size(42, 14);
+            this.ModelLabel.TabIndex = 7;
+            this.ModelLabel.Text = "Model";
+            // 
+            // MakeLabel
+            // 
+            this.MakeLabel.AutoSize = true;
+            this.MakeLabel.Location = new System.Drawing.Point(125, 12);
+            this.MakeLabel.Name = "MakeLabel";
+            this.MakeLabel.Size = new System.Drawing.Size(35, 14);
+            this.MakeLabel.TabIndex = 6;
+            this.MakeLabel.Text = "Make";
+            // 
+            // IDLabel
+            // 
+            this.IDLabel.AutoSize = true;
+            this.IDLabel.Location = new System.Drawing.Point(34, 12);
+            this.IDLabel.Name = "IDLabel";
+            this.IDLabel.Size = new System.Drawing.Size(49, 14);
+            this.IDLabel.TabIndex = 5;
+            this.IDLabel.Text = "Car ID";
+            // 
+            // AddCarButton
+            // 
+            this.AddCarButton.Location = new System.Drawing.Point(282, 26);
+            this.AddCarButton.Name = "AddCarButton";
+            this.AddCarButton.Size = new System.Drawing.Size(84, 22);
+            this.AddCarButton.TabIndex = 4;
+            this.AddCarButton.Text = "Add Car";
+            this.AddCarButton.UseVisualStyleBackColor = true;
+            this.AddCarButton.Click += new System.EventHandler(this.AddCarButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(415, 332);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(515, 332);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DanielsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 367);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CarTableGroupBox);
+            this.Controls.Add(this.ResHistGroupBox);
+            this.Controls.Add(this.DB2DataListBox);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "DanielsForm";
+            this.Text = "DanielsForm";
+            this.ResHistGroupBox.ResumeLayout(false);
+            this.CarTableGroupBox.ResumeLayout(false);
+            this.CarTableGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox DB2DataListBox;
+        private System.Windows.Forms.Button ResHistButton;
+        private System.Windows.Forms.GroupBox ResHistGroupBox;
+        private System.Windows.Forms.GroupBox CarTableGroupBox;
+        private System.Windows.Forms.Button CarsButton;
+        private System.Windows.Forms.Button AddCarButton;
+        private System.Windows.Forms.TextBox ModelTextBox;
+        private System.Windows.Forms.TextBox MakeTextBox;
+        private System.Windows.Forms.TextBox IDTextBox;
+        private System.Windows.Forms.Label ModelLabel;
+        private System.Windows.Forms.Label MakeLabel;
+        private System.Windows.Forms.Label IDLabel;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
---
+++ DanielsForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IBM.Data.DB2.iSeries;
+
+namespace PixisAir_Team3_dg
+{
+    public partial class DanielsForm : Form
+    {
+        // Constants
+        private const string CONNECTION_STRING = ""; // REMOVED
+        private const string RES_TABLE = "Flight2023.ResHist";
+        private const string CAR_TABLE = "Flight2023.Cars";
+
+        // Database Components
+        private iDB2Connection connection = null;
+        private iDB2Command command = null;
+        private iDB2DataReader reader = null;
+
+        public DanielsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RESHISTButton_Click(object sender, EventArgs e)
+        {
+            ClearListBox();
+            String sql = "SELECT * FROM " + RES_TABLE;
+            SelectProcedure(sql);
+        }
+        
+        private void CarsButton_Click(object sender, EventArgs e)
+        {
+            ClearListBox();
+            String sql = "SELECT * FROM " + CAR_TABLE;
+            SelectProcedure(sql);
+        }
+
+        private void SelectProcedure(string sql)
+        {
+            try
+            {
+                connection = new iDB2Connection(CONNECTION_STRING);
+                connection.Open();
+
+                command = new iDB2Command(sql, connection);
+
+                reader = command.ExecuteReader();
+
+                StringBuilder record = new StringBuilder();
+                int fieldSize = reader.FieldCount;
+                while (reader.Read())
+                {
+                    for (int i = 0; i < fieldSize; i++)
+                    {
+                        string trimmed = reader[i].ToString().Trim();
+                        if (trimmed.Length < 10)
+                        {
+                            record.AppendFormat("{0,-12}", trimmed.PadLeft(10, ' '));
+                        }
+                        else
+                        {
+                            record.AppendFormat("{0,-12}", trimmed);
+                        }
+                    }
+
+                    DB2DataListBox.Items.Add(record);
+                    record.Clear();
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                DB2DataListBox.Items.Add(ex);
+            }
+        }
+
+        private void AddCarButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateTextBoxes())
+            {
+                Car car = new Car(int.Parse(IDTextBox.Text), MakeTextBox.Text, ModelTextBox.Text);
+
+                string sql = "INSERT INTO Flight2023.Cars(ID, Make, Model) " +
+                             "VALUES(" + car.ID + ", '" + car.Make + "', '" + car.Model + "')";
+                InsertProcedure(sql);
+            }
+        }
+
+        private void InsertProcedure(string sql)
+        {
+            ClearListBox();
+            try
+            {
+                connection = new iDB2Connection(CONNECTION_STRING);
+                connection.Open();
+
+                command = new iDB2Command(sql, connection);
+                command.ExecuteNonQuery();
+
+                DB2DataListBox.Items.Add("Record Successfully Added.");
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                DB2DataListBox.Items.Add(ex.Message);
+            }
+        }
+
+        private bool ValidateTextBoxes()
+        {
+            bool pass = true;
+
+            // if cannot parse int, is greater than 5 digits, or is less than 0, fail
+            if(!(int.TryParse(IDTextBox.Text, out int i)) || IDTextBox.Text.Length > 5 || i < 0)
+            {
+                ClearListBox();
+                DB2DataListBox.Items.Add("Invalid ID value.");
+                pass = false;     
+            }   
+            // if length is greater than 10 or empty, fail
+            else if(MakeTextBox.Text.Length > 10 || MakeTextBox.Text.Length == 0)
+            {
+                ClearListBox();
+                DB2DataListBox.Items.Add("Invalid Make value.");
+                pass = false;
+            }
+            // if length is greater than 10 or empty, fail
+            else if (ModelTextBox.Text.Length > 10 || ModelTextBox.Text.Length == 0)
+            {
+                ClearListBox();
+                DB2DataListBox.Items.Add("Invalid Model value.");
+                pass = false;
+            }
+
+            return pass;
+        }
+
+        public class Car
+        {
+            public int ID { get; set; }
+            public string Make { get; set; }
+            public string Model { get; set; }
+
+            public Car(int iD, string make, string model)
+            {
+                ID = iD;
+                Make = make;
+                Model = model;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ClearListBox()
+        {
+            if (DB2DataListBox.Items.Count > 0)
+                DB2DataListBox.Items.Clear();
+        }
+    }
+}
